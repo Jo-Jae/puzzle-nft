7f6271d798789224e46ab41e90813d39e5e6c7eb
Jo-Jae <whwodjs11@naver.com>
2024-06-12 15:33:51 +0900

    mint page

--- vite/src/components/PuzzleCard.tsx
+++ vite/src/components/PuzzleCard.tsx
@@ -4,29 +4,29 @@ import { FC, useEffect, useState } from "react";
 
 interface PuzzleCardProps {
   index: number;
-  isMinted: boolean;
+  balance: number;
 }
 
-const PuzzleCard: FC<PuzzleCardProps> = ({ index, isMinted }) => {
-  const [nftMetadata, setMetadata] = useState<NftMetadata>();
+const PuzzleCard: FC<PuzzleCardProps> = ({ index, balance }) => {
+  //   const [nftMetadata, setMetadata] = useState<NftMetadata>();
 
-  const getNftMetadata = async () => {
-    try {
-      const response = await axios.get<NftMetadata>(
-        `${import.meta.env.VITE_METADATA_URI}${index + 1}.json`
-      );
+  //   const getNftMetadata = async () => {
+  //     try {
+  //       const response = await axios.get<NftMetadata>(
+  //         `${import.meta.env.VITE_METADATA_URI}${index + 1}.json`
+  //       );
 
-      setMetadata(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  //       setMetadata(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
 
-  useEffect(() => console.log(nftMetadata), [nftMetadata]);
+  //   useEffect(() => console.log(nftMetadata), [nftMetadata]);
 
-  useEffect(() => {
-    getNftMetadata();
-  }, []);
+  //   useEffect(() => {
+  //     getNftMetadata();
+  //   }, []);
 
   return (
     <GridItem
@@ -35,7 +35,7 @@ const PuzzleCard: FC<PuzzleCardProps> = ({ index, isMinted }) => {
       w={[20, 20, 40]}
       h={[20, 20, 40]}
     >
-      {!isMinted && (
+      {!balance && (
         <Box
           pos="absolute"
           top={0}
--- vite/src/lib/contractAddress.ts
+++ vite/src/lib/contractAddress.ts
@@ -1,1 +1,1 @@
-export const mintContractAddress = "0x5Dd964363804d677A12c23e1f30aBa0d250d2ab3";
+export const mintContractAddress = "0x83ff59A0257C48109F43D6b7c41cbc56Ee09D96f";
